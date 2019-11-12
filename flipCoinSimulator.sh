a7918541ca3619a34a19f18031a175a36f6dba0c
mvinchankar <mvinchankar18@gmail.com>
2019-11-12 11:19:22 +0530

    FlipCoin program to find the output Head or Tail

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,2 +1,12 @@
-#!/bin/bash -x
-echo Hello World
+#!/bin/bash -x 
+read -p "How many times you want to flip the coin: " a
+for(( count=0; count<$a; count++ ))
+do  
+  guess=$((RANDOM%2))
+  if [ 1 -eq $guess ]
+  then 
+      echo TAIL
+  else
+      echo HEAD
+  fi
+done
