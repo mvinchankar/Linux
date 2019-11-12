ff3501f4205fc817d830d1061a1ae627a5079cd8
mvinchankar <mvinchankar18@gmail.com>
2019-11-12 12:31:26 +0530

    Using functions to calculate percentage of head and tail

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,21 +1,32 @@
 #!/bin/bash -x 
-read -p "How many times you want to flip the coin: " numberOfCounts
-for(( count=0; count<$numberOfCounts; count++ ))
+read -p "How many times you want to flip the coin: " numberOfCounts 
+HEAD=0;
+TAIL=0;
+declare -A Dictionary
+function flipCoin()
+{
+
+for(( count=0; count<$1; count++ ))
 do  
   guess=$((RANDOM%2))
   if [ 1 -eq $guess ]
   then 
       TAIL=$(($TAIL+1))
+      Dictionary[((TAIL))]=$TAIL
   else
       HEAD=$(($HEAD+1))
+      Dictionary[((HEAD))]=$HEAD
   fi
 done
-declare -A Dictionary
-Dictionary[((TAIL))]=$TAIL
-Dictionary[((HEAD))]=$HEAD
-echo ${Dictionary[@]}
+}
+function Percentage()
+{
 temp=$(($HEAD * 100))
-HeadPercentage=$(($temp/$numberOfCounts))
+HeadPercentage=$(($temp/$1))
 temp=$(($TAIL * 100))
-TailPercentage=$(($temp/$numberOfCounts))
+TailPercentage=$(($temp/$1))
 echo TailPercentage:$TailPercentage HeadPercentage:$HeadPercentage 
+}
+
+flipCoin $numberOfCounts
+Percentage $numberOfCounts
