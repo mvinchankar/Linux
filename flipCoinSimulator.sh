d840e852d6087b6f9c6dcb365fd477ab42b0e76b
mvinchankar <mvinchankar18@gmail.com>
2019-11-12 12:02:08 +0530

    Program to calculate Head and Tail Percentage and use of Dictionary

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,12 +1,21 @@
 #!/bin/bash -x 
-read -p "How many times you want to flip the coin: " a
-for(( count=0; count<$a; count++ ))
+read -p "How many times you want to flip the coin: " numberOfCounts
+for(( count=0; count<$numberOfCounts; count++ ))
 do  
   guess=$((RANDOM%2))
   if [ 1 -eq $guess ]
   then 
-      echo TAIL
+      TAIL=$(($TAIL+1))
   else
-      echo HEAD
+      HEAD=$(($HEAD+1))
   fi
 done
+declare -A Dictionary
+Dictionary[((TAIL))]=$TAIL
+Dictionary[((HEAD))]=$HEAD
+echo ${Dictionary[@]}
+temp=$(($HEAD * 100))
+HeadPercentage=$(($temp/$numberOfCounts))
+temp=$(($TAIL * 100))
+TailPercentage=$(($temp/$numberOfCounts))
+echo TailPercentage:$TailPercentage HeadPercentage:$HeadPercentage 
