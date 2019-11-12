b25663f59891c5c34fb6d3f09ad069bbc342bb99
mvinchankar <mvinchankar18@gmail.com>
2019-11-12 15:42:19 +0530

    Program to calculate Triplet Coin to flip and also count Percentage

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,9 +1,13 @@
 #!/bin/bash -x 
 read -p "How many times you want to flip the coin: " numberOfCounts 
-HEADHEAD=0;
-TAILTAIL=0;
-HEADTAIL=0;
-TAILHEAD=0;
+HEADHEADHEAD=0;
+TAILTAILTAIL=0;
+HEADTAILTAIL=0;
+TAILHEADHEAD=0;
+HEADTAILHEAD=0;
+TAILHEADTAIL=0;
+TAILTAILHEAD=0;
+HEADHEADTAIL=0;
 declare -A Dictionary
 function flipCoin()
 {
@@ -12,40 +16,75 @@ for(( count=0; count<$1; count++ ))
 do  
   guess=$((RANDOM%2))
   guess1=$((RANDOM%2))
-  if [ $guess -eq $guess1 ]
+  guess2=$((RANDOM%2))
+  if [[ $guess -eq $guess1 ]] && [[ $guess1 -eq $guess2 ]] && [[ $guess -eq $guess2 ]]
   then 
       if [ 1 -eq $guess ]
       then  
-          TAILTAIL=$(($TAILTAIL+1))
-     	  Dictionary[((TAILTAIL))]=$TAILTAIL
+          TAILTAILTAIL=$(($TAILTAILTAIL+1))
+     	  Dictionary[((TAILTAILTAIL))]=$TAILTAILTAIL
       else
-          HEADHEAD=$(($HEADHEAD+1))
-          Dictionary[((HEADHEAD))]=$HEADHEAD
+          HEADHEADHEAD=$(($HEADHEADHEAD+1))
+          Dictionary[((HEADHEADHEAD))]=$HEADHEADHEAD
       fi
   else
-      if [ 1 -eq $guess ]
+      if [ $guess -eq $guess1 ]
       then 
-          TAILHEAD=$(($TAILHEAD+1))
-          Dictionary[((TAILHEAD))]=$TAILHEAD
+          if [ 1 -eq $guess ]
+          then
+              TAILTAILHEAD=$(($TAILTAILHEAD+1))
+              Dictionary[((TAILHEAD))]=$TAILTAILHEAD
+          else 
+              HEADHEADTAIL=$(($HEADHEADTAIL+1))
+              Dictionary[((HEADHEADTAIL))]=$HEADHEADTAIL
+          fi  
       else
-          HEADTAIL=$(($HEADTAIL+1))
-          Dictionary[((HEADTAIL))]=$HEADTAIL  
-      fi  
-      
-  fi
+          if [ 1 -eq $guess ]
+          then
+              if [ $guess1 -eq $guess2 ]
+              then 
+                  TAILHEADHEAD=$(($TAILTAIL+1))
+                  Dictionary[((TAILHEADHEAD))]=$TAILHEADHEAD
+              else
+                  TAILHEADTAIL=$(($TAILHEADTAIL+1))
+                  Dictionary[((TAILHEADTAIL))]=$TAILHEADTAIL
+              fi
+          else
+              if [ $guess1 -eq $guess2 ]
+              then 
+                  HEADTAILTAIL=$(($HEADTAILTAIL+1))
+                  Dictionary[((HEADTAILTAIL))]=$HEADTAILTAIL
+              else
+                  HEADTAILHEAD=$(($HEADTAILTAIL+1))
+                  Dictionary[((HEADTAILHEAD))]=$HEADTAILHEAD 
+              fi
+         fi  
+      fi
+  fi 
 done
 }
 function Percentage()
 {
-temp=$(($HEADHEAD * 100))
-HEADHEADPercentage=$(($temp/$1))
-temp=$(($TAILTAIL * 100))
-TAILTAILPercentage=$(($temp/$1))
-temp=$(($TAILHEAD * 100))
-TAILHEADPercentage=$(($temp/$1))
-temp=$(($HEADTAIL * 100))
-HEADTAILPercentage=$(($temp/$1))
-echo TAILTAILPercentage:$TAILTAILPercentage HEADHEADPercentage:$HEADHEADPercentage HEADTAILPercentage:$HEADTAILPercentage TAILHEADPercentage:$TAILHEADPercentage 
+temp=$(($TAILHEADHEAD * 100))
+TAILHEADHEADPercentage=$(($temp/$1))
+temp=$(($TAILTAILHEAD * 100))
+TAILTAILHEADPercentage=$(($temp/$1))
+temp=$(($TAILTAILTAIL * 100))
+TAILTAILTAILPercentage=$(($temp/$1))
+temp=$(($TAILHEADTAIL * 100))
+TAILHEADTAILPercentage=$(($temp/$1))
+temp=$(($HEADHEADHEAD * 100))
+HEADHEADHEADPercentage=$(($temp/$1))
+temp=$(($HEADTAILTAIL * 100))
+HEADTAILTAILPercentage=$(($temp/$1))
+temp=$(($HEADHEADTAIL * 100))
+HEADHEADTAILPercentage=$(($temp/$1))
+temp=$(($HEADTAILHEAD * 100))
+HEADTAILHEADPercentage=$(($temp/$1))
+echo TAILTAILTAILPercentage:$TAILTAILTAILPercentage HEADHEADHEADPercentage:$HEADHEADHEADPercentage 
+echo HEADTAILTAILPercentage:$HEADTAILTAILPercentage TAILHEADHEADPercentage:$TAILHEADHEADPercentage
+echo TAILTAILHEADPercentage:$TAILTAILHEADPercentage HEADHEADTAILPercentage:$HEADHEADTAILPercentage
+echo TAILHEADTAILPercentage:$TAILHEADTAILPercentage HEADTAILHEADPerentage:$HEADTAILHEADPercentage 
 }
 
 flipCoin $numberOfCounts
